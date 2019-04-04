3d2b2463260e4bc2cd2834f766240a65da79c727
Fernando Poncio <fecrackle@gmail.com>
2019-04-03 23:58:12 -0600

    Agregando Pagina de New Badge

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(
-  <Badge
-    firstName="Fernando"
-    lastName="Poncio"
-    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-    jobTitle="Frontend Engineer"
-    twitter="fherponcio"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
