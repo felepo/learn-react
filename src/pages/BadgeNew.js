acd2f29e2d66ddd0f4a87bec2265b721e96347f4
Fernando Poncio <fecrackle@gmail.com>
2019-04-06 16:20:15 -0600

    Agreganto página de New Badge

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -7,6 +7,25 @@ import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form, //Se obtiene todos los valores que pueda tener el estado
+        [e.target.name]: e.target.value //Se le agrega un nuevo valor
+      }
+    });
+  };
+
   render() {
     return (
       <div>
@@ -20,16 +39,20 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName="Fernando"
-                lastName="Poncio"
-                twitter="fherponcio"
-                jobTitle="Frontend Engineer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://www.gravatar.com/avatar?d=identicon"
               />
             </div>
 
             <div className="col-6">
-              <BadgeForm />
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
         </div>
