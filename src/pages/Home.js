033d63241600fc2aef388a83584e5145277f44f1
Fernando Poncio <fecrackle@gmail.com>
2019-04-21 00:52:39 -0600

    Agregando Pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/Home.css';
-import platziStars from '../images/stars.svg';
+import astronautsImg from '../images/astronauts.svg';
+import platziConfLogo from '../images/platziconf-logo.svg';
 
 function Home() {
   return (
     <div className="Home">
-      <img
-        src={platziStars}
-        alt="Platzi Conf Logo"
-        className="img-fluid Home__img-stars"
-      />
-      <div>Logo</div>
-      <div>Logo Grande</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-4">
+            <img
+              src={platziConfLogo}
+              alt="Platzi Conf Logo"
+              className="img-fluid"
+            />
+
+            <h1 className="Home__h1">Badge Management System</h1>
+
+            <Link to="/badges" className="btn btn-primary Home__btn-start">
+              Start
+            </Link>
+          </div>
+          <div className="col-12 col-sm-8">
+            <img src={astronautsImg} alt="Astronauts" className="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
