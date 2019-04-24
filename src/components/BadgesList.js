37d87806505aff44413347e13b3b8ea7ccf3f141
Fernando Poncio <fecrackle@gmail.com>
2019-04-23 23:44:01 -0600

    Agregando control a los estados de una peticion GET

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/BadgesList.css';
 import twitterLogo from '../images/twitter-logo.svg';
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create New Badge
+          </Link>
+        </div>
+      );
+    }
+
     return (
       <ul className="list-unstyled">
         {this.props.badges.map(badge => {
