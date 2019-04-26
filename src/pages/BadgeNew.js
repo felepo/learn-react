92dd04c73d205144baeb824996582a9da203c8cd
Fernando Poncio <fecrackle@gmail.com>
2019-04-26 00:05:01 -0600

    Controlando respuesta de errores y loading, y generando avatar con algoritmo md5

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,15 +4,19 @@ import './styles/BadgeNew.css';
 import header from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import BadgeForm from '../components/BadgeForm';
-
+import PageLoading from '../components/PageLoading';
 import api from '../api';
+import md5 from 'md5';
 
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: '',
       lastName: '',
       email: '',
+      avatarUrl: '',
       jobTitle: '',
       twitter: ''
     }
@@ -32,15 +36,29 @@ class BadgeNew extends React.Component {
 
     this.setState({ loading: true, error: null });
 
+    //se crea el hash con md5 usando el email para crear el avatar
+    let email = this.state.form.email;
+    let hash = md5(email);
+
     try {
+      //se asigna la direccion de gravatar en el estado de avatarUrl usando el hash que se creo anteriormente
+      this.state.form.avatarUrl = `https://www.gravatar.com/avatar/${hash}?d=identicon`;
+
+      //Se envia los datos
       await api.badges.create(this.state.form);
       this.setState({ loading: false });
+
+      this.props.history.push('/badges');
     } catch (error) {
       this.setState({ loading: false, error: error });
     }
   };
 
   render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
@@ -69,6 +87,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error={this.state.error}
               />
             </div>
           </div>
